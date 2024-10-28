e096bf80c1d393b402c1e6c7d0661e0a1f110595
silgenius <olutademartin@gmail.com>
2024-10-28 11:49:28 +0000

    Implement solution to task 0

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""
+ a function named index_range that takes two integer
+ arguments page and page_size.
+"""
+
+
+def index_range(page, page_size):
+    """
+    return a tuple of size two containing a start index and an end
+    index corresponding to the range of indexes
+    """
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return (start_index, end_index)
