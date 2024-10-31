a5daebcf4e22727329489d1e0b69d9b33254c3f6
silgenius <olutademartin@gmail.com>
2024-10-31 02:24:04 +0000

    Implement solution to task

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+"""
+a class LRUCache that inherits from BaseCaching and is a caching system
+"""
+
+from collections import OrderedDict
+BasicCache = __import__('0-basic_cache').BasicCache
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LRUCache(BasicCache):
+    """
+    Implement the LRU caching algorithm
+    """
+    lru = OrderedDict()
+
+    def __init__(self):
+        """
+        Initialize
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key and item:
+            self.cache_data[key] = item
+            self.lru[key] = item
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                item = self.lru.popitem(last=False)
+                self.cache_data.pop(item[0])
+                print(f'DISCARD: {item[0]}')
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        if self.cache_data.get(key):
+            self.lru.move_to_end(key)
+        return self.cache_data.get(key)
