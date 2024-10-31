12c1e0cb8f309aeaf1015da42a53864c0ce8608e
silgenius <olutademartin@gmail.com>
2024-10-31 01:09:21 +0000

    Implement solution to task 1

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+"""
+a class FIFOCache that inherits from BaseCaching and is a caching system
+"""
+
+BasicCache = __import__('0-basic_cache').BasicCache
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BasicCache):
+    def __init__(self):
+        """
+        Initialize
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key and item:
+            self.cache_data[key] = item
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                first_item = list(self.cache_data.keys())[0]
+                self.cache_data.pop(first_item)
+                print(f'DISCARD: {first_item}')
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        return self.cache_data.get(key)
