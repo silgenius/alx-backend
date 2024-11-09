95beb132366564602b16a793a175a11b9535e5aa
silgenius <olutademartin@gmail.com>
2024-11-09 00:57:51 +0000

    Fix error in task solution file

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,11 +1,22 @@
+#!/usr/bin/env python3
+
+"""
+Strt a flask server
+"""
+
 from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def default():
+    """
+    Default route
+    """
     return render_template('0-index.html')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
