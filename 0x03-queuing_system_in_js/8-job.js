be93b96d78e2df96516c9035a0bf8df6f516046f
silgenius <olutademartin@gmail.com>
2024-12-03 20:28:23 +0000

    Implement solution to task 10

--- 0x03-queuing_system_in_js/8-job.js
+++ 0x03-queuing_system_in_js/8-job.js
@@ -1,10 +1,10 @@
-const createPushNotificationsJobs = (jobs, queue) => {
+export default function createPushNotificationsJobs(jobs, queue) {
 	if (!(jobs instanceof Array)) {
 		throw (new Error('Jobs is not an array'));
 	}
 
 	jobs.forEach((jobData) => {
-		const job = queue.creata('push_notification_code_3', jobData)
+		const job = queue.create('push_notification_code_3', jobData)
 		.save((err) => {
 			if (err) {
 				console.log(`Notification job ${job.id} failed: err`);
