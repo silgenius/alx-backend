f19948721bc8c113bc37d99c43d3a1ef02392445
silgenius <olutademartin@gmail.com>
2024-12-03 21:03:39 +0100

    Implement solution to task 10

--- 0x03-queuing_system_in_js/8-job.js
+++ 0x03-queuing_system_in_js/8-job.js
@@ -1,33 +1,29 @@
-import kue from 'kue';
-
-const queue = kue.createQueue();
-
-const createPushNotificationsJobs(jobs, queue) {
-	if (!(jobs isinstanceof Array)) {
+const createPushNotificationsJobs = (jobs, queue) => {
+	if (!(jobs instanceof Array)) {
 		throw (new Error('Jobs is not an array'));
 	}
 
 	jobs.forEach((jobData) => {
-		const job = queue.Create('push_notification_code_3', jobData)
+		const job = queue.creata('push_notification_code_3', jobData)
 		.save((err) => {
 			if (err) {
-				console.log(`Notification job ${job.id} failed: err);
-			} else
-			{
-				console.log(`Notification job created: ${job.id});
+				console.log(`Notification job ${job.id} failed: err`);
 			}
-		}
+			else {
+				console.log(`Notification job created: ${job.id}`)
+			}
+		});
+		
+		job.on('complete', () => {
+			console.log(`Notification ${job.id} completed`);
+		});
+	
+		job.on('failed', (err) => {
+			console.log(`Notification ${job.id} failed`);
+		});
+	
+		job.on('progress', (progress) => {
+			console.log(`Notification job ${job.id} ${progress}% complete`)
+		});
 	});
 }
-
-job.on('complete', () => {
-        console.log(`Notification ${job.id} completed`);
-    });
-
-    job.on('failed', (err) => {
-        console.log(`Notification ${job.id} failed`);
-    });
-
-    job.on('progress', (progress) => {
-        console.log(`Notification job ${job.id} ${progress}% complete`)
-    });
