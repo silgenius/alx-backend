d22d4ecfa98807ee0197fe97288f52832fef476a
silgenius <olutademartin@gmail.com>
2024-12-05 00:05:40 +0100

    Implement solution to advance task:

---
+++ 0x03-queuing_system_in_js/100-seat.js
@@ -0,0 +1,85 @@
+import redis from 'redis';
+import { promisify } from 'util'
+import kue from 'kue';
+import express from 'express';
+
+const queue = kue.createQueue();
+const redisClient = redis.createClient();
+const app = express();
+
+let reservationEnabled = true;
+
+function reserveSeat(number) {
+    redisClient.set('available_seats', number, redis.print);
+}
+
+const getAsync = promisify(redisClient.get).bind(redisClient);
+
+async function getCurrentAvailableSeats() {
+    try {
+        const result = await getAsync('available_seats');
+    } catch(err) {
+        console.log(err);
+    }
+    return result;
+}
+
+reserveSeat(50); // sets the default number of available seats to 50
+
+app.get('/available_seats', async (req, res) => {
+    const available_seat = await getCurrentAvailableSeats();
+    res.json({ numberOfAvailableSeats: available_seat });
+});
+
+app.get('/reserve_seat', (req, res) => {
+    if (!reservationEnabled) {
+        res.json( { "status": "Reservation are blocked" });
+        return;
+    }
+
+    const job = queue.create('reserve_seat')
+    .save((err) => {
+        if (err) {
+            res.json('{ "status": "Reservation failed" }')
+            return;
+        }
+        res.json({ "status": "Reservation in process" })
+    });
+
+    job.on('complete', () => {
+        console.log('Seat reservation job JOB_ID completed');
+    });
+
+    job.on('failed', (err) => {
+        console.log(`Seat reservation job JOB_ID failed: ${err}`);
+    });
+});
+
+app.get('/process', (req, res) => {
+    queue.process('reserve_seat', async (job, done) => {
+        const available_seat = await getCurrentAvailableSeats();
+        if ((available_seat - 1) === 0) {
+            reservationEnabled = false;
+        }
+
+        if ((available_seat - 1 ) < 0) {
+            done(new Error('Not enough seats available'));
+        }
+
+        reserveSeat(available_seat - 1);
+        done();
+    })
+    res.json( { "status": "Queue processing" });
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://${hostitemName}:${port}/`);
+});
+
+redisClient.on('connect', () => {
+	console.log('Redis client connected to the server');
+});
+
+redisClient.on('error', (err) => {
+	console.log(`Redis client not connected to the server: ${err}`);
+});
